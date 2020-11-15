ce72dda65442256093afcf48f32fc4b1540ae140
Sahil <soilshubham@gmail.com>
2020-11-16 00:48:43 +0530

    late night commit

--- DaaProject.cpp
+++ DaaProject.cpp
@@ -10,21 +10,17 @@ struct kMap
 int main(){
     kMap KMap1[4][4];
     vector<int> posOf1s;
-    int noOf1s;
+    int noOf1s, input, i;
+
+    //Input
     cout<<"Enter number of min/max terms: ";
     cin>>noOf1s;
     cout<<"Enter postions of 1s: ";
-    int input;
     for (int i = 0; i < noOf1s; i++)
-        {
-            cin>>input;
-            if(input>=0 && input<=15)
-            posOf1s.push_back(input);
-        }
-
-    for (int i : posOf1s)
     {
-        cout<<i<<" ";
+        cin>>input;
+        if(input>=0 && input<=15)
+        posOf1s.push_back(input);
     }
     
 }
