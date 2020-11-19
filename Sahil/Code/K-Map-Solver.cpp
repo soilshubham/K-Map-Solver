e3e0f8f5f4b84c6f44598f690c8ab91c66ff836a
Sahil <soilshubham@gmail.com>
2020-11-20 02:35:43 +0530

    group cheching for 8 elements finished

--- Sahil/Code/K-Map-Solver.cpp
+++ Sahil/Code/K-Map-Solver.cpp
@@ -2,6 +2,9 @@
 #include<string.h>
 #include<vector>
 #include<map>
+
+void g8(int a[][4], std::vector<int>*, std::vector<int>*);
+
 struct kMap
 {
     int n = 0;
@@ -11,6 +14,10 @@ struct kMap
 int main(){
 
     int KMap1[4][4];
+    
+    std::vector<int> grp8_i, grp8_j;
+
+    //Sets all values of matrix 0
     for(int i=0; i<4; i++){
         for (int j = 0; j < 4; j++) 
         KMap1[i][j] = 0;
@@ -43,12 +50,14 @@ int main(){
     std::cout<<"Enter number of min/max terms: ";
     std::cin>>noOf1s;
     std::cout<<"Enter postions of min/max terms: ";
-    for (int i = 0; i < noOf1s; i++)
+    for (int i = 0; i < noOf1s; i++)   
     {
         std::cin>>input;
         if(input>=0 && input<=15)
         KMap1[stoi(getIndex[input])/10][stoi(getIndex[input])%10] = 1;
     }
+
+    g8(KMap1, &grp8_i, &grp8_j);
 
     //Output of initial array
     for(int i=0; i<4; i++){
@@ -57,5 +66,64 @@ int main(){
             std::cout<<KMap1[i][j]<<" ";
         }
         std::cout<<std::endl;
+    }
+
+    std::cout<<"Group of 8 starts at index:\n";
+    for (int i : grp8_i)
+    {
+        std::cout<<i<<"["<<i/10<<", "<<i%10<<"]"<<std::endl;
+    }
+
+    for (int i : grp8_j)
+    {
+        std::cout<<i<<"["<<i/10<<", "<<i%10<<"]"<<std::endl;
+    }
+    
+
+}
+
+
+void g8(int a[][4], std::vector<int>* b, std::vector<int>* c){
+
+    int flag = 0;
+    
+    //grp i
+    for (int i=0; i<4 ; i++){
+        for (int j=0; j<4; j++){
+            if (a[i][j]==1) {
+                if (a[i%4][(j+1)%4]==1 and a[(i+1)%4][j%4]==1 and a[(i+1)%4][(j+1)%4]==1) {
+                    flag++;
+                }
+                else {
+                    flag=0;
+                    break;
+                }
+            }
+            if (flag == 3){
+                b->push_back(10*i + abs(j - 2)%4);
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    //grp8 j
+    for (int j=0; j<4 ; j++){
+        for (int i=0; i<4; i++){
+            if (a[i][j]==1) {
+                if (a[i%4][(j+1)%4]==1 and a[(i+1)%4][j%4]==1 and a[(i+1)%4][(j+1)%4]==1) {
+                    flag++;
+                }
+                else {
+                    flag=0;
+                    break;
+                }
+            }
+            if (flag == 3){
+                c->push_back(10*(abs(i - 2)%4) + j);
+                flag = 0;
+                break;
+            }
+        }
     }
 }
--- Sahil/Code/alekhhya-code.cpp
+++ Sahil/Code/alekhhya-code.cpp
@@ -4,19 +4,47 @@
 #include<map>
 using namespace std;
 
-int main (){
+void g8(int* a[], vector<int>* b, vector<int>* c){
+
     int flag = 0;
-    int x[4][4] = {{1,1,1,1}, {1,1,1,1}, {0,0,0,0},{0,0,0,0}};
+    
+
+    //grp i
     for (int i=0; i<4 ; i++){
         for (int j=0; j<4; j++){
-            if (x[i][j]==1) {
-                if (x[i%4][(j+1)%4]==1 and x[(i+1)%4][j%4]==1 and x[(i+1)%4][(j+1)%4]==1) {
+            if (a[i][j]==1) {
+                if (a[i%4][(j+1)%4]==1 and a[(i+1)%4][j%4]==1 and a[(i+1)%4][(j+1)%4]==1) {
                     flag++;
                 }
+                else flag=0;
             }
             if (flag == 3){
-                cout<< 8 ;
+                b->push_back(10*i + j);
+                flag = 0;
+                break;
             }
         }
     }
+
+    //grp8 j
+    for (int j=0; j<4 ; j++){
+        for (int i=0; i<4; i++){
+            if (a[i][j]==1) {
+                if (a[i%4][(j+1)%4]==1 and a[(i+1)%4][j%4]==1 and a[(i+1)%4][(j+1)%4]==1) {
+                    flag++;
+                }
+                else flag=0;
+            }
+            if (flag == 3){
+                c->push_back(10*i + j);
+                flag = 0;
+                break;
+            }
+        }
+    }
+}
+
+int main (){
+    vector<int> grp8_i, grp8_j;
+    int x[4][4] = {{1,1,1,1}, {1,1,1,1}, {0,0,0,0},{0,0,0,0}};
 }
