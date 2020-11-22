ebfcedce18e0b9774ffd2eec1371b39a7eb3c1f2
Sahil <soilshubham@gmail.com>
2020-11-23 02:49:45 +0530

    elimination of unnecessary group of 4
    Also changed the function parameters from pointers to call by reference

--- Sahil/Code/K-Map-Solver.cpp
+++ Sahil/Code/K-Map-Solver.cpp
@@ -1,113 +1,15 @@
+#include<algorithm>
 #include<iostream>
 #include<string.h>
 #include<vector>
 #include<map>
 
-void g8(int a[][4], std::vector<std::vector<int>>*);
-void g4(int a[][4], std::vector<std::vector<int>>*);
-void g2(int a[][4], std::vector<std::vector<int>>*);
-
-struct kMap
-{
-    int n = 0;
-    std::vector<int> group;
-};
-
-int main(){
-
-    int KMap1[4][4];
-    
-    std::vector<std::vector<int>> grp8, grp4, grp2;
-
-    //Sets all values of matrix 0
-    for(int i=0; i<4; i++){
-        for (int j = 0; j < 4; j++) 
-        KMap1[i][j] = 0;
-    }
-    
-    //Map assignment
-    std::map<int, std::string> getIndex;
-    {
-        getIndex[0] = "00";
-        getIndex[1] = "01";
-        getIndex[2] = "03";
-        getIndex[3] = "02";
-        getIndex[4] = "10";
-        getIndex[5] = "11";
-        getIndex[6] = "13";
-        getIndex[7] = "12";
-        getIndex[8] = "30";
-        getIndex[9] = "31";
-        getIndex[10] = "33";
-        getIndex[11] = "32";
-        getIndex[12] = "20";
-        getIndex[13] = "21";
-        getIndex[14] = "23";
-        getIndex[15] = "22";
-    }
-    
-    int noOf1s, input;
-
-    //Input
-    std::cout<<"Enter number of min/max terms: ";
-    std::cin>>noOf1s;
-    std::cout<<"Enter postions of min/max terms: ";
-    for (int i = 0; i < noOf1s; i++)   
-    {
-        std::cin>>input;
-        if(input>=0 && input<=15)
-        KMap1[stoi(getIndex[input])/10][stoi(getIndex[input])%10] = 1;
-    }
-
-    g8(KMap1, &grp8);
-    g4(KMap1, &grp4);
-    g2(KMap1, &grp2);
-
-    //Output of initial array
-    for(int i=0; i<4; i++){
-        for (int j = 0; j < 4; j++)
-        {
-            std::cout<<KMap1[i][j]<<" ";
-        }
-        std::cout<<std::endl;
-    }
-
-    std::cout<<"\nGroup of 8\n";
-    for (std::vector<int> i : grp8)
-    {
-        for(int j : i){
-            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<" , ";
-        }
-        std::cout<<"\n";
-    }
-
-    std::cout<<"\nGroup of 4\n";
-    for (std::vector<int> i : grp4)
-    {
-        for(int j : i){
-            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<" , ";
-        }
-        std::cout<<"\n";
-    }
-    
-    std::cout<<"\nGroup of 2\n";
-    for (std::vector<int> i : grp2)
-    {
-        for(int j : i){
-            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<" , ";
-        }
-        std::cout<<"\n";
-    }
-
-}
-
-
-void g8(int a[][4], std::vector<std::vector<int>>* grp8){
+void g8(int a[][4], std::vector<std::vector<int>> &grp8){
 
     int flag = 0;
     std::vector<int> grp;
 
-    //grp i
+    //horizontal
     for (int i=0; i<4 ; i++){
         for (int j=0; j<4; j++){
             if (a[i][j]==1) {
@@ -130,7 +32,7 @@ void g8(int a[][4], std::vector<std::vector<int>>* grp8){
                 grp.push_back(10*((i+1)%4) + abs(j+2 - 2)%4);
                 grp.push_back(10*((i+1)%4) + abs(j+3 - 2)%4);
 
-                grp8->push_back(grp);
+                grp8.push_back(grp);
                 grp.clear();
                 flag = 0;
                 break;
@@ -138,7 +40,7 @@ void g8(int a[][4], std::vector<std::vector<int>>* grp8){
         }
     }
 
-    //grp8 j
+    //vertical
     for (int j=0; j<4 ; j++){
         for (int i=0; i<4; i++){
             if (a[i][j]==1) {
@@ -161,7 +63,7 @@ void g8(int a[][4], std::vector<std::vector<int>>* grp8){
                 grp.push_back(10*((i+2- 2)%4) + abs((j+1))%4);
                 grp.push_back(10*((i+3- 2)%4) + abs((j+1))%4);
 
-                grp8->push_back(grp);
+                grp8.push_back(grp);
                 grp.clear();
                 flag = 0;
                 break;
@@ -170,7 +72,7 @@ void g8(int a[][4], std::vector<std::vector<int>>* grp8){
     }
 }
 
-void g4(int a[][4], std::vector<std::vector<int>>* grp4){
+void g4(int a[][4], std::vector<std::vector<int>> &grp4){
 
     int flag = 0;
     std::vector<int> grp;
@@ -184,7 +86,7 @@ void g4(int a[][4], std::vector<std::vector<int>>* grp4){
                     grp.push_back(10*((i)%4) + ((j+1)%4));
                     grp.push_back(10*((i+1)%4) + ((j)%4));
                     grp.push_back(10*((i+1)%4) + ((j+1)%4));
-                    grp4->push_back(grp);
+                    grp4.push_back(grp);
                     grp.clear();
                 }
             }
@@ -200,7 +102,7 @@ void g4(int a[][4], std::vector<std::vector<int>>* grp4){
                     grp.push_back(10*((i+1)%4) + ((j)%4));
                     grp.push_back(10*((i+2)%4) + ((j)%4));
                     grp.push_back(10*((i+3)%4) + ((j)%4));
-                    grp4->push_back(grp);
+                    grp4.push_back(grp);
                     grp.clear();
                     break;
                 }
@@ -218,7 +120,7 @@ void g4(int a[][4], std::vector<std::vector<int>>* grp4){
                     grp.push_back(10*((i)%4) + ((j+1)%4));
                     grp.push_back(10*((i)%4) + ((j+2)%4));
                     grp.push_back(10*((i)%4) + ((j+3)%4));
-                    grp4->push_back(grp);
+                    grp4.push_back(grp);
                     grp.clear();
                     break;
                 }
@@ -228,7 +130,7 @@ void g4(int a[][4], std::vector<std::vector<int>>* grp4){
     
 }
 
-void g2(int a[][4], std::vector<std::vector<int>>* grp2){
+void g2(int a[][4], std::vector<std::vector<int>> &grp2){
 
     std::vector<int> grp;
 
@@ -238,16 +140,128 @@ void g2(int a[][4], std::vector<std::vector<int>>* grp2){
                 if (a[i%4][(j+1)%4]==1) {
                     grp.push_back(10*(i%4) + (j)%4);
                     grp.push_back(10*(i%4) + ((j+1))%4);
-                    grp2->push_back(grp);
+                    grp2.push_back(grp);
                     grp.clear();
                 }
                 if (a[(i+1)%4][j%4]==1 ) {
                     grp.push_back(10*((i)%4) + (j)%4);
                     grp.push_back(10*((i+1)%4) + (j)%4);
-                    grp2->push_back(grp);
+                    grp2.push_back(grp);
                     grp.clear();
                 }
             }
         }
     }
+}
+
+void eliminate4(std::vector<std::vector<int>> &grp8, std::vector<std::vector<int>> &grp4){
+
+    std::vector<std::vector<int>> grp4_T;
+
+    int flag=0;
+
+    for(int i=0; i<grp4.size(); i++){
+        std::sort(grp4[i].begin(), grp4[i].end());
+
+        for (int j=0; j<grp8.size(); j++){
+            std::sort(grp8[j].begin(), grp8[j].end());
+
+            if(includes(grp8[j].begin(), grp8[j].end(), grp4[i].begin(), grp4[i].end()))
+                flag++;
+        }
+        if (flag==0)
+            grp4_T.push_back(grp4[i]);
+        else flag=0;
+    }
+    grp4 = grp4_T;
+}
+
+int main(){
+
+    int KMap1[4][4];
+    
+    std::vector<std::vector<int>> grp8, grp4, grp2;
+
+    //Sets all values of matrix 0
+    for(int i=0; i<4; i++){
+        for (int j = 0; j < 4; j++) 
+        KMap1[i][j] = 0;
+    }
+    
+    //Map assignment
+    std::map<int, int> getIndex;
+    {
+        getIndex[0] = 0;
+        getIndex[1] = 1;
+        getIndex[2] = 3;
+        getIndex[3] = 2;
+        getIndex[4] = 10;
+        getIndex[5] = 11;
+        getIndex[6] = 13;
+        getIndex[7] = 12;
+        getIndex[8] = 30;
+        getIndex[9] = 31;
+        getIndex[10] = 33;
+        getIndex[11] = 32;
+        getIndex[12] = 20;
+        getIndex[13] = 21;
+        getIndex[14] = 23;
+        getIndex[15] = 22;
+    }
+    
+    int noOf1s, input;
+
+    //Input
+    std::cout<<"Enter number of min/max terms: ";
+    std::cin>>noOf1s;
+    std::cout<<"Enter postions of min/max terms: ";
+    for (int i = 0; i < noOf1s; i++)   
+    {
+        std::cin>>input;
+        if(input>=0 && input<=15)
+        KMap1[getIndex[input]/10][getIndex[input]%10] = 1;
+    }
+
+    g8(KMap1, grp8);
+    g4(KMap1, grp4);
+    g2(KMap1, grp2);
+    eliminate4(grp8, grp4);
+    
+
+    //Output of initial array
+    for(int i=0; i<4; i++){
+        for (int j = 0; j < 4; j++)
+        {
+            std::cout<<KMap1[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+
+    std::cout<<"\n\nGroup of 8\n";
+    for (std::vector<int> i : grp8)
+    {
+        for(int j : i){
+            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<", ";
+        }
+        std::cout<<"\n";
+    }
+
+    std::cout<<"\nGroup of 4\n";
+    for (std::vector<int> i : grp4)
+    {
+        for(int j : i){
+            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<", ";
+        }
+        std::cout<<"\n";
+    }
+    
+    // std::cout<<"\nGroup of 2\n";
+    // for (std::vector<int> i : grp2)
+    // {
+    //     for(int j : i){
+    //         std::cout<<"["<<j/10<<", "<<j%10<<"]"<<" , ";
+    //     }
+    //     std::cout<<"\n";
+    // }
+
 }
--- Sahil/Code/test.cpp
+++ Sahil/Code/test.cpp
@@ -1,95 +1,19 @@
+#include<algorithm>
 #include<iostream>
 #include<string.h>
 #include<vector>
 #include<map>
 
-void g8(int a[][4], std::vector<std::vector<int>>* grp8);
+// void g8(int a[][4], std::vector<std::vector<int>>*);
+// void g4(int a[][4], std::vector<std::vector<int>>*);
+// void g2(int a[][4], std::vector<std::vector<int>>*);
+// void eliminate4(int a[][4], std::vector<std::vector<int>>, std::vector<std::vector<int>>);
 
 struct kMap
 {
     int n = 0;
     std::vector<int> group;
 };
-
-int main(){
-
-    int KMap1[4][4];
-    
-    std::vector<std::vector<int>> grp8;
-
-    std::vector<int> grp8_i, grp8_j;
-
-    //Sets all values of matrix 0
-    for(int i=0; i<4; i++){
-        for (int j = 0; j < 4; j++) 
-        KMap1[i][j] = 0;
-    }
-    
-    //Map assignment
-    std::map<int, std::string> getIndex;
-    {
-        getIndex[0] = "00";
-        getIndex[1] = "01";
-        getIndex[2] = "03";
-        getIndex[3] = "02";
-        getIndex[4] = "10";
-        getIndex[5] = "11";
-        getIndex[6] = "13";
-        getIndex[7] = "12";
-        getIndex[8] = "30";
-        getIndex[9] = "31";
-        getIndex[10] = "33";
-        getIndex[11] = "32";
-        getIndex[12] = "20";
-        getIndex[13] = "21";
-        getIndex[14] = "23";
-        getIndex[15] = "22";
-    }
-    
-    int noOf1s, input;
-
-    //Input
-    std::cout<<"Enter number of min/max terms: ";
-    std::cin>>noOf1s;
-    std::cout<<"Enter postions of min/max terms: ";
-    for (int i = 0; i < noOf1s; i++)   
-    {
-        std::cin>>input;
-        if(input>=0 && input<=15)
-        KMap1[stoi(getIndex[input])/10][stoi(getIndex[input])%10] = 1;
-    }
-
-    g8(KMap1, &grp8);
-
-    //Output of initial array
-    for(int i=0; i<4; i++){
-        for (int j = 0; j < 4; j++)
-        {
-            std::cout<<KMap1[i][j]<<" ";
-        }
-        std::cout<<std::endl;
-    }
-
-    std::cout<<"\nGroup of 8\n";
-    for (std::vector<int> i : grp8)
-    {
-        for(int j : i){
-            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<" , ";
-        }
-        std::cout<<"\n";
-    }
-    std::cout<<"\nGroup of 4\n";
-    for (std::vector<int> i : grp8)
-    {
-        for(int j : i){
-            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<" , ";
-        }
-        std::cout<<"\n";
-    }
-
-}
-
-
 
 void g8(int a[][4], std::vector<std::vector<int>>* grp8){
 
@@ -175,17 +99,14 @@ void g4(int a[][4], std::vector<std::vector<int>>* grp4){
                     grp.push_back(10*((i+1)%4) + ((j+1)%4));
                     grp4->push_back(grp);
                     grp.clear();
-                    flag = 0;
-                    break;
                 }
-                else flag=0;
             }
         }
     }
     
     //grp vertical line
-    for (int i=0; i<4 ; i++){
-        for (int j=0; j<4; j++){
+    for (int j=0; j<4 ; j++){
+        for (int i=0; i<4; i++){
             if (a[i][j]==1) {
                 if (a[i%4][(j)%4]==1 and a[(i+1)%4][(j)%4]==1 and a[(i+2)%4][(j)%4]==1 and a[(i+3)%4][(j)%4]==1) {
                     grp.push_back(10*((i)%4) + ((j)%4));
@@ -194,10 +115,8 @@ void g4(int a[][4], std::vector<std::vector<int>>* grp4){
                     grp.push_back(10*((i+3)%4) + ((j)%4));
                     grp4->push_back(grp);
                     grp.clear();
-                    flag = 0;
                     break;
                 }
-                else flag=0;
             }
         }
     }
@@ -214,12 +133,151 @@ void g4(int a[][4], std::vector<std::vector<int>>* grp4){
                     grp.push_back(10*((i)%4) + ((j+3)%4));
                     grp4->push_back(grp);
                     grp.clear();
-                    flag = 0;
                     break;
                 }
-                else flag=0;
             }
         }
     }
     
+}
+
+void g2(int a[][4], std::vector<std::vector<int>>* grp2){
+
+    std::vector<int> grp;
+
+    for (int i=0; i<4 ; i++){
+        for (int j=0; j<4; j++){
+            if (a[i][j]==1) {
+                if (a[i%4][(j+1)%4]==1) {
+                    grp.push_back(10*(i%4) + (j)%4);
+                    grp.push_back(10*(i%4) + ((j+1))%4);
+                    grp2->push_back(grp);
+                    grp.clear();
+                }
+                if (a[(i+1)%4][j%4]==1 ) {
+                    grp.push_back(10*((i)%4) + (j)%4);
+                    grp.push_back(10*((i+1)%4) + (j)%4);
+                    grp2->push_back(grp);
+                    grp.clear();
+                }
+            }
+        }
+    }
+}
+
+void eliminate4(std::vector<std::vector<int>> &grp8, std::vector<std::vector<int>> &grp4){
+
+    std::vector<std::vector<int>> grp4_T;
+
+    int flag=0;
+
+    for(int i=0; i<grp4.size(); i++){
+        std::sort(grp4[i].begin(), grp4[i].end());
+
+        for (int j=0; j<grp8.size(); j++){
+            std::sort(grp8[j].begin(), grp8[j].end());
+
+            if(includes(grp8[j].begin(), grp8[j].end(), grp4[i].begin(), grp4[i].end()))
+            {
+                flag++;
+            }
+        }
+        if (flag==0)
+        {
+            grp4_T.push_back(grp4[i]);
+        }
+        else flag=0;
+        
+    }
+    grp4 = grp4_T;
+}
+
+int main(){
+
+    int KMap1[4][4];
+    
+    std::vector<std::vector<int>> grp8, grp4, grp2;
+
+    //Sets all values of matrix 0
+    for(int i=0; i<4; i++){
+        for (int j = 0; j < 4; j++) 
+        KMap1[i][j] = 0;
+    }
+    
+    //Map assignment
+    std::map<int, std::string> getIndex;
+    {
+        getIndex[0] = "00";
+        getIndex[1] = "01";
+        getIndex[2] = "03";
+        getIndex[3] = "02";
+        getIndex[4] = "10";
+        getIndex[5] = "11";
+        getIndex[6] = "13";
+        getIndex[7] = "12";
+        getIndex[8] = "30";
+        getIndex[9] = "31";
+        getIndex[10] = "33";
+        getIndex[11] = "32";
+        getIndex[12] = "20";
+        getIndex[13] = "21";
+        getIndex[14] = "23";
+        getIndex[15] = "22";
+    }
+    
+    int noOf1s, input;
+
+    //Input
+    std::cout<<"Enter number of min/max terms: ";
+    std::cin>>noOf1s;
+    std::cout<<"Enter postions of min/max terms: ";
+    for (int i = 0; i < noOf1s; i++)   
+    {
+        std::cin>>input;
+        if(input>=0 && input<=15)
+        KMap1[stoi(getIndex[input])/10][stoi(getIndex[input])%10] = 1;
+    }
+
+    g8(KMap1, &grp8);
+    g4(KMap1, &grp4);
+    g2(KMap1, &grp2);
+    eliminate4(grp8, grp4);
+    
+
+    //Output of initial array
+    for(int i=0; i<4; i++){
+        for (int j = 0; j < 4; j++)
+        {
+            std::cout<<KMap1[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+
+    std::cout<<"\n\nGroup of 8\n";
+    for (std::vector<int> i : grp8)
+    {
+        for(int j : i){
+            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<", ";
+        }
+        std::cout<<"\n";
+    }
+
+    std::cout<<"\nGroup of 4\n";
+    for (std::vector<int> i : grp4)
+    {
+        for(int j : i){
+            std::cout<<"["<<j/10<<", "<<j%10<<"]"<<", ";
+        }
+        std::cout<<"\n";
+    }
+    
+    // std::cout<<"\nGroup of 2\n";
+    // for (std::vector<int> i : grp2)
+    // {
+    //     for(int j : i){
+    //         std::cout<<"["<<j/10<<", "<<j%10<<"]"<<" , ";
+    //     }
+    //     std::cout<<"\n";
+    // }
+
 }
